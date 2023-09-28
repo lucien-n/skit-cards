4ac885a87e9d11654375240706ac488d68fe5d1f
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-28 16:52:52 +0200

    fix: collection page data fetch

--- src/routes/(app)/collection/[collection_uid]/+layout.ts
+++ src/routes/(app)/collection/[collection_uid]/+layout.ts
@@ -1,9 +1,13 @@
 import { cfetch } from '$lib/cfetch';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async ({ params: { uid }, fetch }) => {
-	const collectionPromise = cfetch<TCollection>(`/api/collections/${uid}`, 'GET', fetch);
-	const cardsPromise = cfetch<TFlashcard[]>(`/api/collections/${uid}/cards`, 'GET', fetch);
+export const load: LayoutLoad = async ({ params: { collection_uid }, fetch }) => {
+	const collectionPromise = cfetch<TCollection>(`/api/collections/${collection_uid}`, 'GET', fetch);
+	const cardsPromise = cfetch<TFlashcard[]>(
+		`/api/collections/${collection_uid}/cards`,
+		'GET',
+		fetch
+	);
 
 	return {
 		streamed: {
