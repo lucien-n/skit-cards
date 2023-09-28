f8e41c1b55772b83d70b36c34aab3abdcf1e0af1
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-28 23:03:31 +0200

    ref: moved collection author to bottom of card

--- src/routes/(app)/collection.svelte
+++ src/routes/(app)/collection.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div
-	class="card border-primary hover-card p-3 w-full h-full flex flex-col justify-center items-center text-center"
+	class="relative card border-primary hover-card p-3 w-full h-full flex flex-col justify-center items-center text-center"
 >
 	<h1 class="text-2xl font-semibold">{collection.name}</h1>
-	<p class="text-foreground/70 text-sm tialic">
+	<p class="text-foreground/70 text-sm tialic absolute bottom-1">
 		by <a class="font-semibold hover:underline" href="/profile/{collection.author.name}"
 			>{collection.author.name}</a
 		>
