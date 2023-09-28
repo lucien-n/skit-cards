389b0ab99ceae803c59c6c4d692d3a6262f0a735
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-28 18:20:40 +0200

    feat: paginator element

---
+++ src/lib/components/cards/paginator.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { cn } from '$utils';
+	import { ChevronLeft, ChevronRight } from 'lucide-svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	export let current: number;
+	export let size: number;
+	export let showArrows: boolean = false;
+	export let controllable: boolean = true;
+
+	const dispatch = createEventDispatcher();
+
+	const change = (index: number) => {
+		if (!controllable) return;
+		current = index;
+		dispatch('change', index);
+	};
+</script>
+
+<div class="relative flex items-center justify-center">
+	<button class="absolute left-0 -translate-x-8" class:hidden={!showArrows || current == 0}>
+		<ChevronLeft />
+	</button>
+	<div class="w-full flex gap-2 items-center">
+		{#each { length: size } as _, i}
+			<button
+				class={cn(
+					'w-3 h-3 rounded-full ease-in-out duration-200',
+					current === i ? 'bg-primary scale-125' : 'bg-primary/60'
+				)}
+				class:bg-primary={current == i}
+				on:click={() => change(i)}
+			/>
+		{/each}
+	</div>
+	<button class="absolute right-0 translate-x-8" class:hidden={!showArrows || current == size}>
+		<ChevronRight />
+	</button>
+</div>
--- src/routes/(app)/collection/[collection_uid]/card-carrousel.svelte
+++ src/routes/(app)/collection/[collection_uid]/card-carrousel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Paginator from '$components/cards/paginator.svelte';
 	import Flashcard from './flashcard.svelte';
 
 	export let cards: TFlashcard[];
@@ -14,4 +15,11 @@
 	const getCurrentCard = () => cards[currentIndex];
 </script>
 
-<Flashcard flashcard={getCurrentCard()} on:next={nextCard} />
+<section class="flex flex-col w-full h-fit items-center gap-3">
+	<Flashcard flashcard={getCurrentCard()} on:next={nextCard} />
+	<Paginator
+		bind:current={currentIndex}
+		size={cards.length}
+		on:change={({ detail }) => (currentIndex = detail)}
+	/>
+</section>
