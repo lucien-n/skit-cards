b78ae39d675a39e97f77440e7e5f8608b43ad0da
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-28 18:43:45 +0200

    ref: get public and self collections when authenticated

--- src/routes/(api)/api/collections/+server.ts
+++ src/routes/(api)/api/collections/+server.ts
@@ -1,14 +1,27 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ locals: { supabase, getSession } }) => {
-	const session = await getSession();
+export const GET: RequestHandler = async ({
+	url: { searchParams },
+	locals: { supabase, getSession }
+}) => {
+	type Mode = 'public' | 'self' | 'both' | null;
+	const which: Mode = searchParams.get('which') as Mode;
 
-	if (!session) return new Response(null, { status: 401 });
+	let match: object;
+	if (which === 'self') {
+		const session = await getSession();
+		if (!session) return new Response(null, { status: 401 });
+		match = { author: session.user.id };
+	} else if (which === 'public') {
+		match = { is_public: true };
+	} else {
+		match = {};
+	}
 
 	const query = supabase
 		.from('cards_collections')
 		.select('uid, author, name, is_public, created_at')
-		.match({ author: session.user.id });
+		.match(match);
 
 	const { data, error, status }: DbResult<typeof query> = await query;
 
--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -1,8 +1,14 @@
 import { cfetch } from '$lib/cfetch';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const collectionsPromise = cfetch<TCollection[]>(`/api/collections`, 'GET', fetch);
+export const load: PageLoad = async ({ fetch, parent, url: { origin } }) => {
+	const { session } = await parent();
+
+	const url = new URL(`${origin}/api/collections`);
+
+	if (session) url.searchParams.set('which', 'both');
+
+	const collectionsPromise = cfetch<TCollection[]>(url.href, 'GET', fetch);
 
 	return {
 		streamed: {
