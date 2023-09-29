14b87f4caf203a0abd65f6e76b5de7f5bd5a7cc4
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-29 12:20:56 +0200

    fix: display collection creation button if no collections

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -27,24 +27,22 @@
 		<ErrorAlert {error} />
 	{/if}
 
-	{#if collections}
-		<section
-			class="w-full md:w-3/4 xl:w-2/3 h-4/5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-5 gap-3"
-		>
+	<section
+		class="w-full md:w-3/4 xl:w-2/3 h-4/5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-5 gap-3"
+	>
+		{#if collections}
 			{#each collections as collection}
 				<a href="/collection/{collection.uid}">
 					<Collection {collection} />
 				</a>
 			{/each}
-			{#if session}
-				<Card class="card hover-card p-0">
-					<a href="/new" class="w-full h-full flex items-center justify-center">
-						<Plus size={21} />
-					</a>
-				</Card>
-			{/if}
-		</section>
-	{:else}
-		<p>No collections</p>
-	{/if}
+		{/if}
+		{#if session}
+			<Card class="card hover-card p-0">
+				<a href="/new" class="w-full h-full flex items-center justify-center">
+					<Plus size={21} />
+				</a>
+			</Card>
+		{/if}
+	</section>
 {/await}
