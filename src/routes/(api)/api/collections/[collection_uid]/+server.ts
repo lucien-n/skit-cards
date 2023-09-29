c2d5b74699582b424f761b6a8121500684c9af5d
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-29 12:20:20 +0200

    fix: properly fetch user profile on collection

--- src/routes/(api)/api/collections/[collection_uid]/+server.ts
+++ src/routes/(api)/api/collections/[collection_uid]/+server.ts
@@ -5,7 +5,7 @@ export const GET: RequestHandler = async ({ params: { collection_uid }, locals: 
 
 	const query = supabase
 		.from('cards_collections')
-		.select('uid, author, name, is_public, created_at')
+		.select('uid, author:profiles(name:name, uid:uid), name, is_public, created_at')
 		.match({ uid: collection_uid });
 
 	const { data, error, status }: DbResult<typeof query> = await query;
@@ -14,8 +14,20 @@ export const GET: RequestHandler = async ({ params: { collection_uid }, locals: 
 
 	if (!data || !(data.length > 0)) return new Response(null, { status: 204 });
 
-	if (data[0] satisfies TCollection)
-		return new Response(JSON.stringify({ data: data[0] }), { status });
-
-	return new Response(null, { status });
+	return new Response(
+		JSON.stringify({
+			data: {
+				...data,
+				author: {
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore
+					uid: collectionData.author.uid,
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore
+					name: collectionData.author.name
+				}
+			}
+		}),
+		{ status }
+	);
 };
