4a07cc367db0762c3de14b82de4819bef65dc409
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-30 19:03:51 +0200

    ref: profile api endpoint rework

--- src/routes/(api)/api/users/[uid]/profile/+server.ts
+++ src/routes/(api)/api/users/[uid]/profile/+server.ts
@@ -1,6 +1,47 @@
 import { getExpiration, getHeaders } from '$server/cache';
 import { redis } from '$server/redis';
+import type { SupabaseClient } from '@supabase/supabase-js';
 import type { RequestHandler } from '@sveltejs/kit';
+
+const getCachedProfile = async (
+	uid?: string,
+	name?: string
+): Promise<{ profile?: TProfile; ttl?: number; status: number }> => {
+	const redisKeyPattern = `profile:${uid ? uid + '|*' : '*|' + name}`;
+	const [_, redisKeys] = await redis.scan(0, 'MATCH', redisKeyPattern);
+	const cached = await redis.get(redisKeys[0]);
+
+	if (cached) {
+		const ttl = await redis.ttl(redisKeys[0]);
+		const profile = JSON.parse(cached);
+		return { profile, ttl, status: 200 };
+	}
+
+	return { status: 404 };
+};
+
+const getProfile = async (supabase: SupabaseClient, uid?: string, name?: string) => {
+	const query = supabase
+		.from('profiles')
+		.select('uid, name, avatar_url, created_at')
+		.match(uid ? { uid } : { name });
+
+	const { data, error, status }: DbResult<typeof query> = await query;
+
+	if (error) return { status };
+
+	if (!data?.[0]) return { status };
+
+	const profile_data = data[0];
+
+	const profile = {
+		uid: profile_data.uid,
+		name: profile_data.name,
+		avatar_url: profile_data.avatar_url || ''
+	} satisfies TPublicProfile;
+
+	return { profile, status };
+};
 
 export const GET: RequestHandler = async ({ params, setHeaders, locals: { supabase } }) => {
 	const uid_or_username = params.uid as string;
@@ -11,34 +52,15 @@ export const GET: RequestHandler = async ({ params, setHeaders, locals: { supaba
 	if (uid_or_username.length === 36) uid = uid_or_username;
 	else name = uid_or_username;
 
-	const redisKeyPattern = `profile:${uid ? uid + '|*' : '*|' + name}`;
-	const [_, redisKeys] = await redis.scan(0, 'MATCH', redisKeyPattern);
-	const cached = await redis.get(redisKeys[0]);
-
-	if (cached) {
-		const ttl = await redis.ttl(redisKeys[0]);
+	const { profile: cachedProfile, ttl, status: cachedStatus } = await getCachedProfile(uid, name);
+	if (cachedProfile) {
 		setHeaders({ 'Cache-Control': `max-age=${ttl}` });
-		return new Response(JSON.stringify({ data: JSON.parse(cached) }), { status: 200 });
+		return new Response(JSON.stringify({ data: cachedProfile }), { status: cachedStatus });
 	}
 
-	const query = supabase
-		.from('profiles')
-		.select('uid, name, avatar_url, created_at')
-		.match(uid ? { uid } : { name });
+	const { profile, status } = await getProfile(supabase, uid, name);
 
-	const { data, error }: DbResult<typeof query> = await query;
-
-	if (error)
-		return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
-	if (!data?.[0]) return new Response(null, { status: 204 });
-
-	const profile_data = data?.[0];
-
-	const profile = {
-		uid: profile_data.uid,
-		name: profile_data.name,
-		avatar_url: profile_data.avatar_url || ''
-	} satisfies TPublicProfile;
+	if (!profile || status !== 200) return new Response(null, { status });
 
 	setHeaders(getHeaders('profile'));
 	redis.set(
