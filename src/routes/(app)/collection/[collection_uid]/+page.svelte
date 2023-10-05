1f5dccd313b213a6e9a2cca67c60407e7f3873ea
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-05 17:53:08 +0200

    ref: separated collection even more

--- src/routes/(app)/collection/[collection_uid]/+page.svelte
+++ src/routes/(app)/collection/[collection_uid]/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import ErrorAlert from '$components/cards/error-alert.svelte';
-	import Button from '$components/ui/button/button.svelte';
 	import { setTitle } from '$lib/helper';
-	import { Plus, Settings } from 'lucide-svelte';
 	import type { PageData } from './$types';
 	import CardCarrousel from './card-carrousel.svelte';
+	import CollectionHeader from './collection-header.svelte';
 	import CardSkeleton from './flashcard-skeleton.svelte';
 
 	export let data: PageData;
@@ -21,15 +20,6 @@
 		const addUrl = new URL(`${$page.url.origin}/collection/${collectionUid}/edit/new`);
 		return addUrl.href;
 	};
-
-	const getSettingsUrl = (collection: TCollection) => {
-		const settingsUrl = new URL(`${$page.url.origin}/collection/${collection.uid}/settings`);
-
-		settingsUrl.searchParams.set('is_public', '' + collection.is_public);
-		settingsUrl.searchParams.set('name', collection.name);
-
-		return settingsUrl.href;
-	};
 </script>
 
 <section class="w-full h-full flex items-center justify-center flex-col relative gap-3">
@@ -39,25 +29,7 @@
 		{#if error}
 			<ErrorAlert {error} />
 		{:else if collection}
-			<div class="flex flex-col w-full justify-center items-center gap-4 top-20 absolute">
-				<h1 class="text-center text-4xl font-semibold">
-					{collection.name}
-				</h1>
-				<div class="flex gap-4">
-					{#if profile && profile.name === collection.author}
-						<Button variant="ghost" href={getSettingsUrl(collection)}>
-							<Settings />
-						</Button>
-						<Button
-							href={getAddUrl(collection.uid)}
-							class="rounded-md text-lg px-5 py-2 bg-primary text-background flex items-center gap-1"
-						>
-							<Plus />
-							Add</Button
-						>
-					{/if}
-				</div>
-			</div>
+			<CollectionHeader {collection} {profile} />
 			{#await cardsPromise}
 				<CardSkeleton />
 			{:then { data: cards, error }}
@@ -66,12 +38,7 @@
 				{:else if cards && cards.length > 0}
 					<CardCarrousel {cards} {profile} author={collection.author} />
 				{:else}
-					<p>
-						No cards in collection. <a
-							href={getAddUrl(collection.uid)}
-							class="font-semibold hover:underline">Start editing</a
-						>
-					</p>
+					<p>No cards in collection.</p>
 				{/if}
 			{/await}
 		{:else}
---
+++ src/routes/(app)/collection/[collection_uid]/collection-header.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Button } from '$components/ui/button';
+	import { Plus, Settings } from 'lucide-svelte';
+
+	export let collection: TCollection;
+	export let profile: TPublicProfile | null;
+
+	const getSettingsUrl = (collection: TCollection) => {
+		const settingsUrl = new URL(`${$page.url.origin}/collection/${collection.uid}/settings`);
+
+		settingsUrl.searchParams.set('is_public', '' + collection.is_public);
+		settingsUrl.searchParams.set('name', collection.name);
+
+		return settingsUrl.href;
+	};
+
+	const getAddUrl = (collectionUid: string) => {
+		const addUrl = new URL(`${$page.url.origin}/collection/${collectionUid}/edit/new`);
+		return addUrl.href;
+	};
+</script>
+
+<div class="flex flex-col w-full justify-center items-center gap-4 top-20 absolute">
+	<h1 class="text-center text-4xl font-semibold">
+		{collection.name}
+	</h1>
+	<div class="flex gap-4">
+		{#if profile && profile.name === collection.author}
+			<Button variant="ghost" href={getSettingsUrl(collection)}>
+				<Settings />
+			</Button>
+			<Button
+				href={getAddUrl(collection.uid)}
+				class="rounded-md text-lg px-5 py-2 bg-primary text-background flex items-center gap-1"
+			>
+				<Plus />
+				Add</Button
+			>
+		{/if}
+	</div>
+</div>
