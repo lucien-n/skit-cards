b6070bb707d1e54951d9b65ce58dd4689c788cdb
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-10 14:03:32 +0200

    wip: user profile

--- src/routes/(app)/profile/[name]/+page.server.ts
+++ src/routes/(app)/profile/[name]/+page.server.ts
@@ -5,7 +5,7 @@ import { cfetch } from '$lib/cfetch';
 export const load: PageServerLoad = async ({ fetch, params: { name } }) => {
 	if (name.length < 3 || name.length > 255) throw redirect(303, '/');
 
-	const profilePromise = cfetch(`/api/users/${name}/profile`, 'GET', fetch);
+	const profilePromise = cfetch<TPublicProfile>(`/api/users/${name}/profile`, 'GET', fetch);
 
 	return {
 		streamed: { profilePromise }
--- src/routes/(app)/profile/[name]/+page.svelte
+++ src/routes/(app)/profile/[name]/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import ErrorAlert from '$components/cards/error-alert.svelte';
+	import * as Card from '$components/ui/card';
 	import { setTitle } from '$lib/helper';
 	import { Loader2 } from 'lucide-svelte';
 	import type { PageServerData } from './$types';
+	import Separator from '$components/ui/separator/separator.svelte';
 
 	export let data: PageServerData;
 
@@ -13,23 +15,31 @@
 	setTitle('Profile');
 </script>
 
-{#await profilePromise}
-	<div class="w-full h-full flex justify-center items-center">
+<div class="w-full h-full flex justify-center items-center">
+	{#await profilePromise}
 		<span class="animate-spin">
 			<Loader2 />
 		</span>
-	</div>
-{:then { data: profile, error }}
-	{#if error || !profile}
-		<ErrorAlert error={error || 'Profile not found'} title="Not Found" />
-	{:else}
-		<section class="gap-2 grid grid-cols-3">
-			{#each Object.entries(profile) as [key, value]}
-				<p class="font-semibold text-right">{key}:</p>
-				<p class="text-left w-full col-span-2">{value || '🤷‍♂️'}</p>
-			{/each}
-		</section>
-	{/if}
-{:catch}
-	<ErrorAlert error="An error occured" title="Internal Error" />
-{/await}
+	{:then { data: profile, error }}
+		{#if error || !profile}
+			<ErrorAlert error={error || 'Profile not found'} title="Not Found" />
+		{:else}
+			<Card.Root class="w-full">
+				<Card.Header class="p-8">
+					<div class="grid grid-rows-2 grid-cols-4 w-fit gap-6">
+						<picture class="row-span-2 col-span-1">
+							<div class="w-24 h-24 aspect-square bg-muted rounded-full" />
+						</picture>
+						<div class="col-span-3 text-left p-2">
+							<h1 class="text-3xl font-bold">{profile.name}</h1>
+						</div>
+					</div>
+				</Card.Header>
+				<Separator />
+				<Card.Content />
+			</Card.Root>
+		{/if}
+	{:catch}
+		<ErrorAlert error="An error occured" title="Internal Error" />
+	{/await}
+</div>
